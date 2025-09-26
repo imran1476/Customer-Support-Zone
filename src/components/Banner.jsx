c5dd772b635cd54fc26789ccfb551ede0f4a469f
Imran1476 <imranlf2028@gmail.com>
2025-09-27 00:51:20 +0600

    Add latest 5 commits

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -4,12 +4,18 @@ import vector1 from '../assets/vector1.png'
 const Banner = ({ inProgress = 0, resolved = 0 }) => {
   return (
     <section className="pt-25 grid grid-cols-1 md:grid-cols-2 gap-6">
+<<<<<<< HEAD
        <div className="h-[200px] text-center flex justify-center items-center rounded-lg p-6 bg-gradient-to-r from-purple-600 to-purple-400 text-white shadow-md">
          
       <div className="text-xl">In-Progress <br /><span className="mt-6 text-5xl font-bold">{inProgress}</span>
       
     </div>
         
+=======
+      <div className="rounded-lg p-6 bg-gradient-to-r from-purple-600 to-purple-400 text-white shadow-md">
+        <div className="text-sm">In-Progress</div>
+        <div className="mt-6 text-4xl font-bold">{inProgress}</div>
+>>>>>>> ae3db9b (navbar)
       </div>
 
       <div className="h-[200px] text-center flex justify-center items-center rounded-lg p-6 bg-gradient-to-r from-green-500 to-emerald-500 text-white shadow-md">
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,7 +5,11 @@ const Navbar = () => {
 <header className="bg-white shadow-sm fixed top-0 left-0 w-full">
 <div className=" mx-auto px-4 py-4 flex items-center justify-between">
 <div className="flex items-center gap-3">
+<<<<<<< HEAD
 <div className="text-sm md:text-2xl lg:4xl font-bold text-[#130B2D] ml-10">CS — Ticket System</div>
+=======
+<div className="text-2xl font-bold text-[#130B2D] ml-10">CS — Ticket System</div>
+>>>>>>> ae3db9b (navbar)
  </div>
 
 <div className="hidden md:flex items-center gap-6">
@@ -18,7 +22,11 @@ const Navbar = () => {
 <a className="hover:text-indigo-600" href="#">Contact</a>
 </nav>
 
+<<<<<<< HEAD
 <button className="bg-violet-600 bg-gradient-to-r from-[#9f62f2] to-[#632ee3] text-white px-4 py-2 rounded-md text-sm mr-10 hover:bg-blue-900"> + New Ticket</button>
+=======
+<button className="bg-violet-600 text-white px-4 py-2 rounded-md text-sm mr-10 hover:bg-blue-900"> + New Ticket</button>
+>>>>>>> ae3db9b (navbar)
 </div>
 
 <div className="md:hidden">
