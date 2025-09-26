ae3db9bf5b296caab2a8e1abd98439315c4868d1
Imran1476 <imranlf2028@gmail.com>
2025-09-26 16:18:31 +0600

    navbar

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Banner = ({ inProgress = 0, resolved = 0 }) => {
   return (
-    <section className="mt-6 grid grid-cols-1 md:grid-cols-2 gap-6">
+    <section className="pt-25 grid grid-cols-1 md:grid-cols-2 gap-6">
       <div className="rounded-lg p-6 bg-gradient-to-r from-purple-600 to-purple-400 text-white shadow-md">
         <div className="text-sm">In-Progress</div>
         <div className="mt-6 text-4xl font-bold">{inProgress}</div>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,33 +2,30 @@ import React from "react";
 
 const Navbar = () => {
   return (
-    <header className="bg-white shadow-sm">
-      <div className="max-w-[1200px] mx-auto px-4 py-4 flex items-center justify-between">
-        <div className="flex items-center gap-3">
-          <div className="text-2xl font-bold text-indigo-700">CS — Ticket System</div>
-        </div>
+<header className="bg-white shadow-sm fixed top-0 left-0 w-full">
+<div className=" mx-auto px-4 py-4 flex items-center justify-between">
+<div className="flex items-center gap-3">
+<div className="text-2xl font-bold text-[#130B2D] ml-10">CS — Ticket System</div>
+ </div>
 
-        <div className="hidden md:flex items-center gap-6">
-          <nav className="flex gap-6 text-sm">
-            <a className="hover:text-indigo-600" href="#">Home</a>
-            <a className="hover:text-indigo-600" href="#">FAQ</a>
-            <a className="hover:text-indigo-600" href="#">Blog</a>
-            <a className="hover:text-indigo-600" href="#">Contact</a>
-          </nav>
+<div className="hidden md:flex items-center gap-6">
+<nav className="flex gap-6 text-sm">
+<a className="hover:text-indigo-600" href="#">Home</a>
+<a className="hover:text-indigo-600" href="#">FAQ</a>
+<a className="hover:text-indigo-600" href="#">Changelog</a>
+<a className="hover:text-indigo-600" href="#">Blog</a>
+<a className="hover:text-indigo-600" href="#">Download</a>
+<a className="hover:text-indigo-600" href="#">Contact</a>
+</nav>
 
-          <button className="bg-violet-600 text-white px-4 py-2 rounded-md text-sm">
-            + New Ticket
-          </button>
-        </div>
+<button className="bg-violet-600 text-white px-4 py-2 rounded-md text-sm mr-10 hover:bg-blue-900"> + New Ticket</button>
+</div>
 
-        
-        <div className="md:hidden">
-          <button className="bg-violet-600 text-white px-3 py-2 rounded-md text-sm">
-            + New
-          </button>
-        </div>
-      </div>
-    </header>
+<div className="md:hidden">
+<button className="bg-violet-600 text-white px-3 py-2 rounded-md text-sm"> + New</button>
+</div>
+</div>
+ </header>
   );
 };
 
